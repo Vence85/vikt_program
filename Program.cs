9ffac3fd12306b6a760a1b41f975135dfe065d53
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2025-02-17 10:06:42 +0100

    Made a small and easy meny

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isrunning = true;
+
+while(isrunning)
+{
+    Console.WriteLine("Vad vill du göra");
+    Console.WriteLine("[1]Lägg in vikt");
+    Console.WriteLine("[2]Kolla medelvikt de senaste 7 dagarna");
+    Console.WriteLine("[3]Kolla lägsta vikt");
+    Console.WriteLine("[Q]Avsluta");
+
+    Console.ReadLine();
+    
+}
