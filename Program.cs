b884d193b2a8bdf93d40a0a4fcb7d60b3bc8efb2
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2025-02-21 07:35:28 +0100

    started on the 'add wheigt' alternative.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,53 @@
-﻿bool isrunning = true;
+﻿
 
-while(isrunning)
+using System.ComponentModel;
+
+bool isrunning = true;
+
+List<double> viktlista = new List<double>();
+while (isrunning)
+
 {
-    Console.WriteLine("Vad vill du göra");
+    Console.Clear();
+    Console.WriteLine("==============================================");
     Console.WriteLine("[1]Lägg in vikt");
     Console.WriteLine("[2]Kolla medelvikt de senaste 7 dagarna");
     Console.WriteLine("[3]Kolla lägsta vikt");
     Console.WriteLine("[Q]Avsluta");
+    Console.WriteLine("==============================================");
 
-    Console.ReadLine();
-    
+    string menyInput = Console.ReadLine();
+
+    switch (menyInput)
+    {
+        case "1":
+            Console.WriteLine("Ange vikt eller skriv 'Q' för att go tillbaka till menyn:");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "q")
+            {
+                break;
+            }
+            if (double.TryParse(input, out double vikt))
+            {
+                viktlista.Add(vikt);
+                Console.WriteLine($"Du lade in vikten {vikt} i listan.");
+                Console.WriteLine("Tryck 'D' för att ta bort den inlagda vikten eller 'B' för att bekrafta:");
+
+                                  
+            }
+            else
+            {
+                Console.WriteLine("Felaktig inmatning, försök igen!");
+            }            
+            break;
+
+        case "2":
+            break;
+
+        case "3":
+            break;
+
+        case "q":
+            break;
+    }
 }
